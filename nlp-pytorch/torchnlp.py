dd5d970f08c35645810ae42ea0ad527c334ca810
Dmitri Soshnikov <dmitri@soshnikov.com>
2021-04-07 18:21:52 +0300

    Fix computer vision and nlp modules, add requirements.txt

--- nlp-pytorch/torchnlp.py
+++ nlp-pytorch/torchnlp.py
@@ -1,14 +1,32 @@
+import builtins
 import torch
 import torchtext
+import collections
 import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def load_dataset(ngrams=1):
-    train_dataset, test_dataset = torchtext.datasets.text_classification.DATASETS['AG_NEWS'](root='./data', vocab=None,ngrams=ngrams)
+vocab = None
+tokenizer = None
+
+def load_dataset(ngrams=1,min_freq=1):
+    global vocab, tokenizer
+    print("Loading dataset...")
+    train_dataset, test_dataset = torchtext.datasets.AG_NEWS(root='./data')
+    train_dataset = list(train_dataset)
+    test_dataset = list(test_dataset)
     classes = ['World', 'Sports', 'Business', 'Sci/Tech']
-    vocab_size = len(train_dataset.get_vocab())
-    return train_dataset,test_dataset,classes,vocab_size
+    print('Building vocab...')
+    tokenizer = torchtext.data.utils.get_tokenizer('basic_english')
+    counter = collections.Counter()
+    for (label, line) in train_dataset:
+        counter.update(torchtext.data.utils.ngrams_iterator(tokenizer(line),ngrams=ngrams))
+    vocab = torchtext.vocab.Vocab(counter, min_freq=min_freq)
+    return train_dataset,test_dataset,classes,vocab
+
+def encode(x,voc=None,unk=0,tokenizer=tokenizer):
+    v = vocab if voc is None else voc
+    return [v.stoi.get(s,unk) for s in tokenizer(x)]
 
 def train_epoch(net,dataloader,lr=0.01,optimizer=None,loss_fn = torch.nn.CrossEntropyLoss(),epoch_size=None, report_freq=200):
     optimizer = optimizer or torch.optim.Adam(net.parameters(),lr=lr)
@@ -33,37 +51,43 @@ def train_epoch(net,dataloader,lr=0.01,optimizer=None,loss_fn = torch.nn.CrossEn
             break
     return total_loss.item()/count, acc.item()/count
 
-def padify(b):
+def padify(b,voc=None,tokenizer=tokenizer):
     # b is the list of tuples of length batch_size
     #   - first element of a tuple = label, 
     #   - second = feature (text sequence)
-    # first, compute max length of a sequence in this minibatch
-    l = max([len(x[1]) for x in b]) 
+    # build vectorized sequence
+    v = [encode(x[1],voc=voc,tokenizer=tokenizer) for x in b]
+    # compute max length of a sequence in this minibatch
+    l = max(map(len,v))
     return ( # tuple of two tensors - labels and features
-        torch.LongTensor([t[0] for t in b]),
-        torch.stack([torch.nn.functional.pad(torch.tensor(t[1]),(0,l-len(t[1])),mode='constant',value=0) for t in b])
+        torch.LongTensor([t[0]-1 for t in b]),
+        torch.stack([torch.nn.functional.pad(torch.tensor(t),(0,l-len(t)),mode='constant',value=0) for t in v])
     )
 
-def offsetify(b):
+def offsetify(b,voc=None):
     # first, compute data tensor from all sequences
-    x = [t[1] for t in b] 
+    x = [torch.tensor(encode(t[1],voc=voc)) for t in b]
     # now, compute the offsets by accumulating the tensor of sequence lengths
-    o = [0] + [len(t[1]) for t in b]
+    o = [0] + [len(t) for t in x]
     o = torch.tensor(o[:-1]).cumsum(dim=0)
     return ( 
-        torch.LongTensor([t[0] for t in b]), # labels
+        torch.LongTensor([t[0]-1 for t in b]), # labels
         torch.cat(x), # text 
         o
     )
 
-def train_epoch_emb(net,dataloader,lr=0.01,optimizer=None,loss_fn = torch.nn.CrossEntropyLoss(),epoch_size=None, report_freq=200):
+def train_epoch_emb(net,dataloader,lr=0.01,optimizer=None,loss_fn = torch.nn.CrossEntropyLoss(),epoch_size=None, report_freq=200,use_pack_sequence=False):
     optimizer = optimizer or torch.optim.Adam(net.parameters(),lr=lr)
     loss_fn = loss_fn.to(device)
     net.train()
     total_loss,acc,count,i = 0,0,0,0
     for labels,text,off in dataloader:
         optimizer.zero_grad()
-        labels,text,off = labels.to(device), text.to(device), off.to(device)
+        labels,text = labels.to(device), text.to(device)
+        if use_pack_sequence:
+            off = off.to('cpu')
+        else:
+            off = off.to(device)
         out = net(text, off)
         loss = loss_fn(out,labels) #cross_entropy(out,labels)
         loss.backward()
